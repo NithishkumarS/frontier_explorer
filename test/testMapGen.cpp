dd9dfb5d1a174e94a129a58ff8bd3cb2f5b27ec7
Nithish <nithishtimes@gmail.com>
2018-12-04 20:22:50 -0500

    [Task:13.9] Unit test to check Node activation of the Get_Map_Node

---
+++ test/testMapGen.cpp
@@ -0,0 +1,63 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2018, Nantha Kumar Sunder, Nithish Sanjeev Kumar
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *  contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**@file testMapGen.cpp
+ *
+ * @brief To test the class mapGen
+ *
+ * @driver Nithish Sanjeev Kumar
+ * @navigator Nantha Kumar Sunder
+ * @copyright 2018 , Nantha Kumar Sunder, Nithish Sanjeev Kumar All rights reserved
+
+ */
+
+#include <gtest/gtest.h>
+#include <ros/ros.h>
+#include <ros/service_client.h>
+#include "../include/map.hpp"
+#include "../include/BFS.hpp"
+
+
+TEST(requestMap, checkNodeActivation) {
+// Creating a Node Handle 
+ros::NodeHandle nh;
+// Bool variable to check the output
+bool output;
+// Creating a map object
+map mapObj;
+// Calling the requestMap using map object
+output = mapObj.requestMap(nh);
+ros::spinOnce();
+// Checking whether the output is true or not
+// if not this test case is passed
+EXPECT_EQ(1, output);
+}
