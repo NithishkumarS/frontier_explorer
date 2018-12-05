cc22084a0bffa3636f5c3789db9849af7245acfd
Nithish <nithishtimes@gmail.com>
2018-12-04 20:11:17 -0500

    [Task:14.3] Added fucntion to find the list of neighbours of any given point

--- src/BFS.cpp
+++ src/BFS.cpp
@@ -26,4 +26,26 @@ int BFS::getDistance(int x, int y) {
   return distance[x][y];
 }
 
+void BFS::neighbourList(const std::vector<std::vector<int>>& frontierVector ,
+                    std::vector<int> v , std::queue<std::vector<int> >& list ){
+  int xPoint = v[0];
+  int yPoint = v[1];
+  std::vector<int> tempVec;
+  int move[3] = {1, -1 ,0};
+
+
+  for (auto & x : move) {
+      for (auto & y : move) {
+            tempVec.push_back(yPoint+y);
+            tempVec.push_back(xPoint+x);
+            for (auto & k : frontierVector) {
+                   if (k == tempVec) {
+                     list.push(k);
+                   }
+             }
+        }
+  }
+}
+
+
 BFS::~BFS() {}
